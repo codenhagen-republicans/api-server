33ed8676056eaeb85023bbb94dab1c118394bd29
Richard <rbanyi@me.com>
2018-11-20 17:29:32 +0100

    Flask-SQLAlchemy connection to docker postgresSQL db;

--- api/app.py
+++ api/app.py
@@ -2,13 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://green_api:green_api@localhost/green_api'
+db = SQLAlchemy(app)
 
+class Test(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    other_thing = db.Column(db.String(80))
+
+    def __str__(self):
+        return str(self.id)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    test = Test(other_thing="Hello world")
+    db.session.add(test)
+    db.session.commit()
+    return str(Test.query.all())
 
 
 app.run(debug=True)
