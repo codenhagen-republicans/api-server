3b564d0e6ebb451ddf89ce86a5b7f3f733e4f20b
Richard <rbanyi@me.com>
2018-11-23 22:40:39 +0200

    translate string from csv

--- api/app.py
+++ api/app.py
@@ -4,13 +4,19 @@
 from flask import Flask
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
+from google.cloud import translate
 import kesko
 import json
+import csv
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://green_api:green_api@localhost/green_api'
 db = SQLAlchemy(app)
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'googletranslate.json'
+translate_client = translate.Client()
 
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,11 +30,33 @@ class Food(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    en_loc = db.Column(db.String(80), nullable=False)
+    sv_loc = db.Column(db.String(80), nullable=False)
     co2_impresion = db.Column(db.Float(), nullable=False)
     co2_transport_import = db.Column(db.Float(), nullable=True)
 
     def __str__(self):
         return str(self.name)
+
+
+@app.route('/loadcsv')
+def load_food():
+    source = open('../db/dummy_co2.csv', 'r')
+    reader = csv.reader(source)
+    line_count = 0
+    for row in reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            english = translate_client.translate(row[0], target_language='en')['translatedText']
+            swedish = translate_client.translate(row[0], target_language='sv')['translatedText']
+            record = Food(name=row[0],en_loc=english,sv_loc=swedish,
+                            co2_impresion=float(row[1]),
+                            co2_transport_import=(float(row[2]) if row[2] != 'null' else None))
+            db.session.add(record)
+            db.session.commit()
+            line_count += 1
+    return 'Load Done'
 
 @app.route('/')
 def hello_world():
