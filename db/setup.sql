96c8a2dc2f169f679840a52db70a81e6ec87da5f
Richard <rbanyi@me.com>
2018-11-23 20:23:21 +0200

    import dummy data

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,9 @@
 create user green_api with login password 'green_api';
 
+GRANT pg_read_server_files TO green_api;
+
+drop database if exists green_api;
+
 create database green_api
     encoding 'UTF8'
     owner green_api;
@@ -11,3 +15,35 @@ create table test (
     id              bigserial    primary key,
     other_thing     text
 );
+
+drop table if exists food;
+
+create table food_raw (
+  name text,
+  co2_impresion text not null,
+  co2_transport_import text default null
+);
+
+COPY food_raw from '/docker-entrypoint-initdb.d/dummy_co2.csv'
+  DELIMITER ',' CSV HEADER QUOTE '"';
+
+create table food (
+  id serial primary key,
+  name text,
+  co2_impresion numeric not null,
+  co2_transport_import numeric default null
+);
+
+insert into food (
+  name,
+  co2_impresion,
+  co2_transport_import
+)
+select
+  name,
+  cast(co2_impresion as float),
+  case
+    when co2_transport_import = 'null'
+      then null
+    else cast(co2_transport_import as float) end
+from food_raw;
