dde8339d55dc492f27788ff375e05d86ecfe6637
sunenilausen <sunenilausen@gmail.com>
2018-11-24 10:07:38 +0200

    default port

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def footprint():
 
     return json.dumps(products, ensure_ascii=False).encode("utf8")
 
-app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
